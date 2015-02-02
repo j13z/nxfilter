41d9db24736cb8dc369196df050463d7fa35b7dd
Johannes Schmitz <j13z@users.noreply.github.com>
2015-02-02 23:54:46 +0100

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,13 @@
+'use strict';
+/* global describe, it */
+/* exported should */
+
+var should = require('chai').should();
+var expect = require('chai').expect;
+
+
+describe('…', function () {
+	it('…', function () {
+
+	});
+});
